f04804207125bae91fec39386c5e61d7482fb972
Dan Woodson <woodson.dan@gmail.com>
2024-07-19 12:12:39 -0600

    Adding missing trackOrg functions

--- app/util/trackOrganization.ts
+++ app/util/trackOrganization.ts
@@ -30,6 +30,7 @@ export const getAlbum = (
 /** Album art cache to avoid repetitve fetches */
 const albumArtCache = new Map<string, Promise<string | null>>();
 
+// XXX: Move to ID3 module
 /** Fetch album art */
 export const getAlbumArt = (files: Files, albumId: string) => {
   if (!albumArtCache.has(albumId)) {
@@ -57,4 +58,38 @@ export const getAlbumArt = (files: Files, albumId: string) => {
 /** Get artist data from files object */
 export const getArtist = (files: Files, artistId: string) => {
   return files[artistId];
+};
+
+/** Given a track's URL, pull data from it to determine the track's artist, album, and number */
+export const getParentDataFromTrackUrl = (trackUrl: string | null) => {
+  const currentTrackPieces = trackUrl ? trackUrl.split("/") : null;
+  const artistName =
+    currentTrackPieces && currentTrackPieces[currentTrackPieces.length - 3];
+  const albumName =
+    currentTrackPieces && currentTrackPieces[currentTrackPieces.length - 2];
+  const trackPieces =
+    currentTrackPieces &&
+    currentTrackPieces[currentTrackPieces.length - 1].split("__");
+  const trackName = trackPieces && trackPieces[1];
+  const trackNumber = trackPieces && trackPieces[0];
+
+  return {
+    artistName,
+    albumName,
+    trackName,
+    trackNumber,
+  };
+};
+
+/** Given files and a track URL, get the following tracks on the album */
+export const getRemainingAlbumTracks = (files: Files, trackUrl: string) => {
+  const { artistName, albumName } = getParentDataFromTrackUrl(trackUrl);
+  const album = getAlbum(files, `${artistName}/${albumName}`);
+  if (album) {
+    const currentTrackIndex = album.tracks.findIndex((t) => t.url === trackUrl);
+    const remainingAlbumTracks = album.tracks.slice(currentTrackIndex + 1);
+    return remainingAlbumTracks;
+  }
+
+  return [];
 };
