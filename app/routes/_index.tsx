4a2d9b368cea35e3a5df499aec052e5c0a2477d8
Dan Woodson <woodson.dan@gmail.com>
2024-07-20 20:59:04 -0600

    Loading files server-side for the index roue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,20 @@
 /** @file Main content to display when at "/" */
-import type { Context } from "../root";
 import type { Files } from "../util/files";
 
 import AlbumTile from "../components/AlbumTile";
 import HorizontalRowWithTitle from "../components/HorizontalRowWithTitle";
 import { getAlbumIdsByRecent } from "../util/files";
-import { useOutletContext } from "@remix-run/react";
+import { getUploadedFiles } from "../util/s3.server";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const files = await getUploadedFiles();
+
+  const recentAlbumIds = getAlbumIdsByRecent(files).slice(0, 5);
+
+  return json({ files, recentAlbumIds });
+};
 
 const ContinueListeningRow = ({ files }: { files: Files }) => (
   <HorizontalRowWithTitle title="Continue Listening">
@@ -13,9 +22,13 @@ const ContinueListeningRow = ({ files }: { files: Files }) => (
   </HorizontalRowWithTitle>
 );
 
-const LatestRow = ({ files }: { files: Files }) => {
-  const albumIds = getAlbumIdsByRecent(files).slice(0, 5);
-
+const LatestRow = ({
+  albumIds,
+  files,
+}: {
+  albumIds: ReturnType<typeof getAlbumIdsByRecent>;
+  files: Files;
+}) => {
   return (
     <HorizontalRowWithTitle title="Latest">
       {albumIds.map((a) => (
@@ -27,8 +40,11 @@ const LatestRow = ({ files }: { files: Files }) => {
 
 const FavoritesRow = ({ files }: { files: Files }) => (
   <HorizontalRowWithTitle title="Favorites">
-    <AlbumTile files={files} albumId="Dance Party Time Machine/Love Shack" />
-    <AlbumTile files={files} albumId="Dance Party Time Machine/Love Shack" />
+    <AlbumTile
+      files={files}
+      albumId="Joe Russo's Almost Dead/2019-08-29 Morrison, CO"
+    />
+    <AlbumTile files={files} albumId="Pearl Jam/Dark Matter" />
     <AlbumTile files={files} albumId="Dance Party Time Machine/Love Shack" />
     <AlbumTile files={files} albumId="Dance Party Time Machine/Love Shack" />
     <AlbumTile files={files} albumId="Dance Party Time Machine/Love Shack" />
@@ -37,13 +53,19 @@ const FavoritesRow = ({ files }: { files: Files }) => (
 
 /** Default (/) content */
 const Index = () => {
-  const { files } = useOutletContext<Context>();
+  const {
+    files,
+    recentAlbumIds,
+  }: {
+    files: Files;
+    recentAlbumIds: ReturnType<typeof getAlbumIdsByRecent>;
+  } = useLoaderData<typeof loader>();
 
   if (files) {
     return (
       <>
         <ContinueListeningRow files={files} />
-        <LatestRow files={files} />
+        <LatestRow files={files} albumIds={recentAlbumIds} />
         <FavoritesRow files={files} />
       </>
     );
