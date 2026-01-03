cc67a72c5b976bc9fd87ce09187f764fe352f01d
Dan Woodson <woodson.dan@gmail.com>
2026-01-02 22:58:37 -0700

    Add placeholder test for PlayerControlsCustomElement

---
+++ app/components/Layout/PlayerControls/player-controls-custom-element.test.ts
@@ -0,0 +1,7 @@
+/** @file Tests for PlayerControlsCustomElement */
+
+import { describe, test } from "vitest";
+
+describe("PlayerControlsCustomElement", () => {
+  test.todo("should be implemented");
+});
